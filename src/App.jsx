c1786e01700bf0d650f7aeed93c8607cff18b579
yashashree27 <yashashree37252@gmail.com>
2026-08-07 15:58:57 +0530

    importing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Body from './components/Body'
 import Login from './components/Login'
 import Profile from './components/Profile'
